97853ed40615d76af42e394f1edc039c43acd4ab
Tyler Place <tylerlplace@gmail.com>
2023-12-26 14:04:29 -0800

    Added /about page sections to navbar

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,16 +10,6 @@ export default function About() {
 
     return (
         <main className="flex flex-col items-center justify-between p-4 pt-8 sm:p-12 pb-0 sm:pb-0 gap-12 relative">
-            <div className="w-full max-w-5xl">
-                <h2 className="text-2xl mb-2">Jump To:</h2>
-                <ul className="markdown list-disc ml-4">
-                    {(config.sections.map((section: any) => (
-                        <li key={section.id}>
-                            <Link href={`/about#`+section.id} dangerouslySetInnerHTML={parseMDInline(section.title)}></Link>
-                        </li>
-                    )))}
-                </ul>
-            </div>
             {(config.sections.map((section: any) => (
                 <div id={section.id} className="max-w-5xl flex flex-col items-center" key={section.id}>
                     <h1 className="text-4xl mb-6 markdown" dangerouslySetInnerHTML={parseMDInline(section.title)}></h1>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import NavbarItem from './components/navbar/NavbarItem'
 import NavbarDropdown from './components/navbar/NavbarDropdown'
 import NavbarDropdownItem from './components/navbar/NavbarDropdownItem'
 import AppearanceToggle from './components/navbar/AppearenceToggle'
+import { getConfig } from './util/configReader'
 
 const font = Poppins({weight: ["400","600","700"], subsets: ["latin-ext"]})
 
@@ -20,14 +21,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const aboutConfig = getConfig('about.yml');
   return (
     <html lang="en" className='dark'>
       <body className={font.className+' min-h-screen bg-slate-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100'}>
         <Navbar>
           <NavbarItem title="Home" href="/"></NavbarItem>
-          <NavbarItem title="About" href="/about"></NavbarItem>
+          <NavbarDropdown title="About" href="/about" pages="/about">
+            {aboutConfig.sections.map((section: any) => (
+              <NavbarDropdownItem title={section.title} href={"/about#"+section.id} key={section.id}></NavbarDropdownItem>
+            ))}
+          </NavbarDropdown>
           <NavbarItem title="Sponsors" href="/sponsors"></NavbarItem>
-          <NavbarDropdown title="Resources" href={["/robot", "/team", "/calendar"]}>
+          <NavbarDropdown title="Resources" pages={["/robot", "/team", "/calendar"]}>
             <NavbarDropdownItem title="Our Robot" href="/robot"></NavbarDropdownItem>
             <NavbarDropdownItem title="Our Team" href="/team"></NavbarDropdownItem>
             <NavbarDropdownItem title="Calendar" href="/calendar"></NavbarDropdownItem>

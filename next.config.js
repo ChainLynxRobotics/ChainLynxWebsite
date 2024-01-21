9e53dfd3ce0fba5718d42eaeba9ba31614e02e78
Tyler Place <79611123+1withspaghetti@users.noreply.github.com>
2024-01-21 12:56:30 -0800

    Added short link redirects

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const fs = require("fs");
+const yaml = require("yaml");
+
+module.exports = {
+    async redirects() {
+
+        const configText = fs.readFileSync("./config/redirects.yml", "utf8");
+        const config = yaml.parse(configText);
+
+        return config.redirects.map((redirect) => {
+            return {
+                source: redirect.source,
+                destination: redirect.destination,
+                permanent: true,
+            }
+        });
+    }
+}
